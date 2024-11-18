ed9f6334d2c39ed042760c10401326d338210977
Terungwa_Ayerhan <terayerhan@outlook.com>
2024-11-18 16:48:14 +1100

    Add MessageQueue<TrafficLightPhase> private member

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -64,6 +64,7 @@ private:
     std::condition_variable _condition;
     std::mutex _mutex;
     TrafficLightPhase _currentPhase;
+    MessageQueue<TrafficLightPhase> _trafficLightPhasesQueue;
 };
 
 #endif
