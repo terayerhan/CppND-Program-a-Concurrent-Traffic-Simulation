d169d770363f7525ce9dcbe6015afda849e6e7cc
Terungwa_Ayerhan <terayerhan@outlook.com>
2024-11-18 16:36:27 +1100

    Implement MessageQueue<T>::send()

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -4,7 +4,7 @@
 
 /* Implementation of class "MessageQueue" */
 
-/* 
+ 
 template <typename T>
 T MessageQueue<T>::receive()
 {
@@ -18,8 +18,11 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+    std::lock_guard<std::mutex> lock(_mutex);
+    _queue.push_back(std::move(msg));
+    _condition.notify_one();  // notify the receiver after pushing new message in the queue
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
